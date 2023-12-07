39b40f717733eb7c510c9e846fbc65c8ff3cf8e4
wahidquadri <151563446+wahidquadri@users.noreply.github.com>
2023-12-07 11:20:40 +0530

    Add files via upload

---
+++ ope1.py
@@ -0,0 +1,16 @@
+# Addition Process
+
+a = 10
+b = 20.5
+c = "vahid"
+d = True
+e = False
+f = "khan"
+
+
+print(a+b) #30.5
+print(a+c) #error
+print(a+d) #11
+print(a+e) #10
+print(a+f) #error
+print(c+f) #vahidkhan
---
+++ ope2.py
@@ -0,0 +1,16 @@
+# Multiplication Process 
+
+
+a = 10
+b = 20.5
+c = "vahid"
+d = True
+e = False
+f = "khan"
+
+
+print(a*b) # 205
+print(a*c) # vahid vahid vahid ..........
+print(a*d) # 10
+print(a*e) # 0
+print(a*f) # khan khan khan .....
