a350ab24179e1ab3145cf4db9a07db1a45653530
wahidquadri <151563446+wahidquadri@users.noreply.github.com>
2023-12-12 01:11:42 +0530

    Add files via upload

---
+++ JS-1.js
@@ -0,0 +1,4 @@
+var a = 10
+var b = "vk"
+document.write(a+b)
+document.write(typeof a)
---
+++ js-2.js
@@ -0,0 +1,9 @@
+var a = 10
+var b = 20.2
+var c = "vahid"
+var d = true
+var f ;
+document.write(a+b)
+document.write(a-c)
+document.write(a*b)
+document.write(a*f)
