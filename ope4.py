585f7e649d4288448262a1a90a984a556b035617
Dhur Wahid Quadri <151563446+wahidquadri@users.noreply.github.com>
2023-12-28 11:43:55 +0530

    Add files via upload

---
+++ ope4.py
@@ -0,0 +1,15 @@
+# Member ship Model
+# In type operator
+
+#a = 10,20,20
+#b = 102,102,103
+#print(10 in a)
+#print(101 in b)
+
+
+# not In type operator
+
+a =["vahid"]
+b = [121,123,1234,1212]
+print(121 not in b)
+print('z'not in a)
